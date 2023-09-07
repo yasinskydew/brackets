ce267eb7f6733b36eb4e3a5773146bf1211f09d4
yasinskyvladimir <uladzimir.yasinski@twinslash.com>
2023-09-07 13:24:40 +0300

    feat: set brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const openBrackets = new Map();
+  const closeBrackets = new Map();
+  let equalBrackets = new Map();
+  let index = 0;
+
+  for (const [open, close] of bracketsConfig) {
+    openBrackets.set(open, close);
+    closeBrackets.set(close, open);
+  }
+
+  if(!!(str.length % 2)) return false;
+
+  for (const char of str) {
+    if (openBrackets.has(char)) {
+      if (openBrackets.get(char) == char && !equalBrackets.has(char)) {
+        equalBrackets.set(char, index);
+      } else {
+        stack.push(char);
+      }
+    }
+    
+    if (closeBrackets.has(char)) {
+      if (
+        stack[stack.length - 1] === closeBrackets.get(char)
+      ) {
+        if (closeBrackets.get(char) == char) {
+          const equalIndex = equalBrackets.get(char);
+          if(equalIndex !== index && !!((index - equalIndex) % 2)) {
+            equalBrackets.delete(char)
+            stack.pop();
+          }
+        } else {
+          stack.pop();
+        }
+      }
+    }
+    index++;
+  }
+  return stack.length === 0;
 }
